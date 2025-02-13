7a4922523ff5e742566ac36dc6488160efafe5ae
Venoxexec <ngeshtonsnugglypopo1@gmail.com>
2025-02-13 17:34:48 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+let editors = {};
+
+require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.33.0/min/vs' }});
+require(['vs/editor/editor.main'], function() {
+    editors['tab1'] = monaco.editor.create(document.getElementById('tab1'), {
+        value: '',
+        language: 'javascript',
+        theme: 'vs-dark'
+    });
+    editors['tab2'] = monaco.editor.create(document.getElementById('tab2'), {
+        value: '',
+        language: 'javascript',
+        theme: 'vs-dark'
+    });
+    editors['tab3'] = monaco.editor.create(document.getElementById('tab3'), {
+        value: '',
+        language: 'javascript',
+        theme: 'vs-dark'
+    });
+    openTab('tab1');
+});
+
+function openTab(tabName) {
+    for (let editor in editors) {
+        document.getElementById(editor).style.display = 'none';
+        editors[editor].layout();
+    }
+    document.getElementById(tabName).style.display = 'block';
+    editors[tabName].layout();
+}
