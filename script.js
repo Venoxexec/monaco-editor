2eed07ec724f7a79e620733fd0656abb03265ccc
Venoxexec <ngeshtonsnugglypopo1@gmail.com>
2025-02-13 17:40:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ let editors = {};
 require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.33.0/min/vs' }});
 require(['vs/editor/editor.main'], function() {
     editors['tab1'] = monaco.editor.create(document.getElementById('tab1'), {
-        value: '',
+        value: '// Type your code here...',
         language: 'javascript',
         theme: 'vs-dark'
     });
     editors['tab2'] = monaco.editor.create(document.getElementById('tab2'), {
-        value: '',
+        value: '// Type your code here...',
         language: 'javascript',
         theme: 'vs-dark'
     });
     editors['tab3'] = monaco.editor.create(document.getElementById('tab3'), {
-        value: '',
+        value: '// Type your code here...',
         language: 'javascript',
         theme: 'vs-dark'
     });
